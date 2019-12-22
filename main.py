d4f876dc8aa03018e74e530608f1164fa5a45adf
LeviCC8 <leviccxd@gmail.com>
2019-12-22 15:20:48 -0300

    Added Glucose3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from problem_modeling import generate_model
-from utilities_tests import fill_tests, accuracy
+from utilities_tests import fill_tests, performance
 
 patients = np.genfromtxt('data/column_bin.csv', delimiter=',', skip_header=3)
 patients_input = patients[:, :-1]
@@ -14,4 +14,9 @@ RULES_NUMBER = 3  # It's necessary at least 2
 
 generate_model(patients_input, patients_output, RULES_NUMBER, 'model.txt')
 fill_tests('model.txt', test_input)
-accuracy(test_output)
+
+hit_rate, mean_run_time = performance(test_output, my_method=True)
+print(f'### My method ###\nHit rate = {hit_rate}\nMean Run time = {mean_run_time} miliseconds')
+
+hit_rate, mean_run_time = performance(test_output, my_method=False)
+print(f'### Glucose3 ###\nHit rate = {hit_rate}\nMean Run time = {mean_run_time} miliseconds')
--- utilities_tests.py
+++ utilities_tests.py
@@ -1,5 +1,7 @@
 from DPLL import DPPL
 from utilities_DIMACS_CNF import append_clauses, read_DIMACS_CNF, write_solution
+import time
+from pysat.solvers import Glucose3
 
 
 def fill_tests(model_name, input):
@@ -10,12 +12,27 @@ def fill_tests(model_name, input):
         append_clauses(model_name, new_clauses, f'Test {n}', f'tests/test_{n}.txt')
 
 
-def accuracy(output):
+def performance(output, my_method):
     test_predict = []
+    run_time = []
     for i in range(1, 156):
         A = read_DIMACS_CNF(f'tests/test_{i}.txt')
-        answer = DPPL(A)
-        write_solution(answer, f'tests/test_{i}_solution.txt')
+        if my_method:
+            start = time.time()
+            answer = DPPL(A)
+            end = time.time()
+            write_solution(answer, f'tests/test_{i}_my_solution.txt')
+        else:
+            glucose = Glucose3()
+            for clause in A:
+                glucose.add_clause(list(clause))
+            start = time.time()
+            answer = glucose.solve()
+            end = time.time()
+            answer = answer if answer is False else glucose.get_model()
+            write_solution(answer, f'tests/test_{i}_glucose3_solution.txt')
         test_predict += [0] if answer is False else [1]
+        run_time.append((end - start) * 1000.0)
     hit_rate = [x == y for x, y in zip(output, test_predict)].count(True) / len(output)
-    print(hit_rate)  # 0.8709677419354839
+    mean_run_time = sum(run_time) / len(run_time)
+    return hit_rate, mean_run_time
